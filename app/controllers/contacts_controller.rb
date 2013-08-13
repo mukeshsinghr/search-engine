48b162ba93d0d30ebb942099c5d098ae56b913d5
Mukesh <mukesh@mukesh-K55VM.(none)>
2013-08-13 20:44:14 +0530

    minor update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     end
     #    end
     @contacts = request.env['omnicontacts.contacts']
-    puts "List of contacts obtained from #{params[:importer]}:"
+    puts "List of contacts obtained from: #{params[:importer]}:"
     @contacts.each do |contact|
       puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
     end
